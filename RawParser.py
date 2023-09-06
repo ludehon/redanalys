62b4ffe273f6b3b2e8c1ecba69dd97eb42fd86ac
ludehon <luciendehon@gmail.com>
2023-09-06 22:35:56 +0200

    update RawParser parsing

--- RawParser.py
+++ RawParser.py
@@ -11,40 +11,54 @@ def read_json_file(file_path):
     return json.loads(data)
 
 
-def append_post_to_file(file_name, s1, s2):
-    try:
-        with open(file_name, 'a') as file:
-            file.write("author:\n")
-            file.write(s1 + '\n')
-            file.write("comments:\n")
-            file.write(s2 + '\n')
-            file.write("------\n")
-    except Exception as e:
-        print(f"Error: {e}")
-
-
 class RawParser:
     def __init__(self):
         pass
 
-    def parse_posts_and_write(self, raw_file_path, parsed_file_path):
-        raw_data = read_json_file(raw_file_path)
-        subs = set(raw_data["subList"])
-        data = raw_data["data"]
-        for sub_name in subs:
-            print(f"parsing sub {sub_name}")
-            posts = data[sub_name]
-            for post_id, post in posts.items():
-                print(f"post {post_id}")
-                post_text = post["selftext"]
-                comments_list = []
-                for comment_id, comment in post["comments"].items():
-                    comments_list.append(comment["body"])
-                append_post_to_file(parsed_file_path, post_text, ". ".join(comments_list))
+    def concatenate_fields(self, data):
+        content = ""
+        if "title" in data:
+            content += data["title"] + " "
+        if "selftext" in data:
+            content += data["selftext"] + " "
+        if "comments" in data:
+            comments = " ".join(comment["body"] for comment in data["comments"].values())
+            content += comments
+        return content.strip()
 
+    """
+    Read the original JSON data from input_file
+    Extract the necessary information and create the new structure
+    Write the transformed JSON data to output_file
+    """
+    def transform_json(self, input_file, output_file):
+        with open(input_file, 'r') as f:
+            original_data = json.load(f)
 
+        sub_list = original_data["subList"][0] if original_data["subList"] else None
+        main_entry = next(iter(original_data["data"][sub_list].values())) if sub_list else None
 
+        if sub_list and main_entry:
+            transformed_data = {
+                "date": original_data["date"],
+                "subList": original_data["subList"],
+                "data": {
+                    sub_list: {
+                        key: {
+                            "content": self.concatenate_fields(value)
+                        }
+                        for key, value in original_data["data"][sub_list].items()
+                    }
+                }
+            }
+
+            with open(output_file, 'w') as f:
+                json.dump(transformed_data, f, indent=4)
+
+"""
+arg1: input file
+"""
 if __name__ == "__main__":
     print(f"Launched on {datetime.now().strftime('%y%m%dT%H%M%S')} with args={sys.argv}")
     rp = RawParser()
-    parsed_data = rp.parse_posts_and_write(sys.argv[1], f"{sys.argv[1]}_parsed.txt")
+    rp.transform_json(sys.argv[1], "output.json")
